fc173db068ef4b205877cbdf1f79adbd894142df
Elkin35 <elkurrocu98@outlook.com>
2023-02-08 10:17:35 -0500

    Ahora se usan palabras reservadas

--- lexerRobot.py
+++ lexerRobot.py
@@ -7,9 +7,22 @@ import ply.lex as lex
 tokens_final = []
 
 # Lista de tokens, en esta lista se deben agragar todos los Tookens que se vayan a utilizar en las funciones
-reservadas = ['START','END','IF','THEN','WHILE','DO','OD', 'CALL','CONST',
-		'VARS','PROCS','OUT','IN','ELSE', 'ROBOT_R', "SOUTH", "O", "NORTH", "WEST", "EAST",
-		"REPEAT", "COMMAND", "CONDITION", "PIPE", "DIRECTION", "OBJECT"
+reservadas = ['ROBOT_R','END','IF','THEN','WHILE','DO', 'CALL','CONST',
+		'VARS','PROCS','OUT','IN','ELSE', 
+		
+		"SOUTH", "O", "NORTH", "WEST", "EAST",
+		"REPEAT", "COMMAND", "CONDITION", "PIPE", "DIRECTION", "OBJECT", 
+		
+		"LEFT", "RIGHT","FRONT", "BACK", 
+		
+		"ASSIGNTO", "GOTO", "MOVE", "TURN", "FACE", "PUT", "PICK",
+		"MOVETOTHE", "MOVEINDIR", "JUMPTOTHE", "JUMPINDIR", "NOP",
+
+		"FACING", "CANPUT", "CANPICK", "CANMOVEINDIR", "CANJUMPINDIR",
+		"CANMOVETOTHE", "CANJUMPTOTHE", "NOT"
+
+		"CHIPS", "BALLOONS",
+
 		]
 
 tokens = reservadas+['ID','NUMBER','PLUS','MINUS','TIMES','DIVIDE',
@@ -60,61 +73,65 @@ t_COLON = r':'
 t_DOT = r'\.'
 t_UPDATE = r':='
 
-# Detecta el inicio del programa (es decir la palabra reservada "ROBOT_R")
-def t_START(t):
-	r'[Rr][Oo][Bb][Oo][Tt]_[Rr]'
-	if t.value.upper() in reservadas:
-		t.value = t.value.upper()
-		#reservadas.get(t.value,'ID')
-		t.type = "START"
-	return t
+
+
+# # Detecta el inicio del programa (es decir la palabra reservada "ROBOT_R")
+# def t_START(t):
+# 	r'[Rr][Oo][Bb][Oo][Tt]_[Rr]'
+# 	if t.value.upper() in reservadas:
+# 		t.value = t.value.upper()
+# 		#reservadas.get(t.value,'ID')
+# 		t.type = "START"
+# 	return t
 
 
 
-# Detecta los puntos cardinales
-def t_O(t):
-	r'\|[Ss][Oo][Uu][Tt][Hh]|[nN][Oo][Rr][Tt][Hh]|[Ww][Ee][Ss][Tt]|[Ee][Aa][Ss][Tt]|[Ss][Oo][Uu][Tt][Hh]'
-	if t.value.upper() in reservadas:
-		t.value = t.value.upper()
-		#reservadas.get(t.value,'ID')
-		t.type = "O"
-	return t
+# # Detecta los puntos cardinales
+# def t_O(t):
+# 	r'\|[Ss][Oo][Uu][Tt][Hh]|[nN][Oo][Rr][Tt][Hh]|[Ww][Ee][Ss][Tt]|[Ee][Aa][Ss][Tt]|[Ss][Oo][Uu][Tt][Hh]'
+# 	if t.value.upper() in reservadas:
+# 		t.value = t.value.upper()
+# 		#reservadas.get(t.value,'ID')
+# 		t.type = "O"
+# 	return t
 
-# Detecta las direcciones
-def t_DIRECTION(t):
-	r'\|[Ll][Ee][Ff][Tt]|[Rr][Ii][Gg][Hh][Tt]|[Ff][Rr][Oo][Nn][Tt]|[Bb][Aa][Cc][Kk]|[Ll][Ee][Ff][Tt]'
-	if t.value.upper() in reservadas:
-		t.value = t.value.upper()
-		#reservadas.get(t.value,'ID')
-		t.type = "DIRECTION"
-	return t
+# # Detecta las direcciones
+# def t_DIRECTION(t):
+# 	r'\|[Ll][Ee][Ff][Tt]|[Rr][Ii][Gg][Hh][Tt]|[Ff][Rr][Oo][Nn][Tt]|[Bb][Aa][Cc][Kk]|[Ll][Ee][Ff][Tt]'
+# 	if t.value.upper() in reservadas:
+# 		t.value = t.value.upper()
+# 		#reservadas.get(t.value,'ID')
+# 		t.type = "DIRECTION"
+# 	return t
 
-# Detecta los comandos
-def t_COMMAND(t):
-	r'\|[Aa][Ss][Ss][Ii][Gg][Nn][Tt][Oo]:|[Gg][Oo][Tt][Oo]:|[Mm][Oo][Vv][Ee]:|[Tt][Uu][Rr][Nn]:|[Ff][Aa][Cc][Ee]:|[Pp][Uu][Tt]:|[Pp][Ii][Cc][Kk]:|[Mm][Oo][Vv][Ee][Tt][Oo][Tt][Hh][Ee]:|[Mm][Oo][Vv][Ee][Ii][Nn][Dd][Ii][Rr]:|[Jj][Uu][Mm][Pp][Tt][Oo][Tt][Hh][Ee]:|[Jj][Uu][Mm][Pp][Ii][Nn][Dd][Ii][Rr]:|[Nn][Oo][Pp]:|[Aa][Ss][Ss][Ii][Gg][Nn][Tt][Oo]:'
-	if t.value.upper() in reservadas:
-		t.value = t.value.upper()
-		#reservadas.get(t.value,'ID')
-		t.type = "COMMAND"
-	return t
+# # Detecta los comandos
+# def t_COMMAND(t):
+# 	r'\|[Aa][Ss][Ss][Ii][Gg][Nn][Tt][Oo]:|[Gg][Oo][Tt][Oo]:|[Mm][Oo][Vv][Ee]:|[Tt][Uu][Rr][Nn]:|[Ff][Aa][Cc][Ee]:|[Pp][Uu][Tt]:|[Pp][Ii][Cc][Kk]:|[Mm][Oo][Vv][Ee][Tt][Oo][Tt][Hh][Ee]:|[Mm][Oo][Vv][Ee][Ii][Nn][Dd][Ii][Rr]:|[Jj][Uu][Mm][Pp][Tt][Oo][Tt][Hh][Ee]:|[Jj][Uu][Mm][Pp][Ii][Nn][Dd][Ii][Rr]:|[Nn][Oo][Pp]:|[Aa][Ss][Ss][Ii][Gg][Nn][Tt][Oo]:'
+# 	if t.value.upper() in reservadas:
+# 		t.value = t.value.upper()
+# 		#reservadas.get(t.value,'ID')
+# 		t.type = "COMMAND"
+# 	return t
 
-# Detecta las condiciones
-def t_CONDITION(t):
-	r'\|[Ff][Aa][Cc][Ii][Nn][Gg]:|[Cc][Aa][Nn][Pp][Uu][Tt]:|[Cc][Aa][Nn][Pp][Ii][Cc][Kk]:|[Cc][Aa][Nn][Mm][Oo][Vv][Ee][Ii][Nn][Dd][Ii][Rr]:|[Cc][Aa][Nn][Jj][Uu][Mm][Pp][Ii][Nn][Dd][Ii][Rr]:|[Cc][Aa][Nn][Mm][Oo][Vv][Ee][Tt][Oo][Tt][Hh][Ee]:|[Cc][Aa][Nn][Jj][Uu][Mm][Pp][Tt][Oo][Tt][Hh][Ee]:|[Nn][Oo][Tt]:|[Ff][Aa][Cc][Ii][Nn][Gg]:'
-	if t.value.upper() in reservadas:
-		t.value = t.value.upper()
-		#reservadas.get(t.value,'ID')
-		t.type = "CONDITION"
-	return t
+# # Detecta las condiciones
+# def t_CONDITION(t):
+# 	r'\|[Ff][Aa][Cc][Ii][Nn][Gg]:|[Cc][Aa][Nn][Pp][Uu][Tt]:|[Cc][Aa][Nn][Pp][Ii][Cc][Kk]:|[Cc][Aa][Nn][Mm][Oo][Vv][Ee][Ii][Nn][Dd][Ii][Rr]:|[Cc][Aa][Nn][Jj][Uu][Mm][Pp][Ii][Nn][Dd][Ii][Rr]:|[Cc][Aa][Nn][Mm][Oo][Vv][Ee][Tt][Oo][Tt][Hh][Ee]:|[Cc][Aa][Nn][Jj][Uu][Mm][Pp][Tt][Oo][Tt][Hh][Ee]:|[Nn][Oo][Tt]:|[Ff][Aa][Cc][Ii][Nn][Gg]:'
+# 	if t.value.upper() in reservadas:
+# 		t.value = t.value.upper()
+# 		#reservadas.get(t.value,'ID')
+# 		t.type = "CONDITION"
+# 	return t
 
-# Detecta los objetos (Chips y Balloons)
-def t_OBJECT(t):
-	r'\|[Cc][Hh][Ii][Pp][Ss]|[Bb][Aa][Ll][Ll][Oo][Oo][Nn][Ss]|[Cc][Hh][Ii][Pp][Ss]'
-	if t.value.upper() in reservadas:
-		t.value = t.value.upper()
-		#reservadas.get(t.value,'ID')
-		t.type = "OBJECT"
-	return t
+# # Detecta los objetos (Chips y Balloons)
+# def t_OBJECT(t):
+# 	r'\|[Cc][Hh][Ii][Pp][Ss]|[Bb][Aa][Ll][Ll][Oo][Oo][Nn][Ss]|[Cc][Hh][Ii][Pp][Ss]'
+# 	if t.value.upper() in reservadas:
+# 		t.value = t.value.upper()
+# 		#reservadas.get(t.value,'ID')
+# 		t.type = "OBJECT"
+# 	return t
+
+
 
 # Detecta los ID's de las variables (en este se guarda todo lo que no se guarda en la  otras funciones)
 def t_ID(t):
